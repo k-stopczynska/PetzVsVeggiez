f8a0a7f71d785ed9898ceaeb4ff224b202705021
k.stopczynska <k.stopczynska@gmail.com>
2024-11-14 18:36:32 +0100

    making ManaPet more reusable

--- core/src/main/java/com/ainspiring/entities/ManaPet.java
+++ core/src/main/java/com/ainspiring/entities/ManaPet.java
@@ -6,11 +6,14 @@ import com.badlogic.gdx.math.Vector2;
 public class ManaPet extends Entity {
 
     protected final int mana;
+    protected final float interval;
     private float elapsedTime = 0f;
     
-    public ManaPet(Texture image, Vector2 position, int health, int cost, int mana) {
+    public ManaPet(Texture image, Vector2 position, int health, int cost, int mana, float interval) {
         super(image, position, health, cost);
-        this.mana = mana;}
+        this.mana = mana;
+        this.interval = interval;
+    }
 
     @Override
     public void update(float delta) {
@@ -18,15 +21,15 @@ public class ManaPet extends Entity {
         throw new UnsupportedOperationException("Unimplemented method 'update'");
     }
 
-    public int generateMana(float delta) {
+    public float generateMana(float delta) {
         if (this.health > 0) {
             elapsedTime += delta;
-            if (elapsedTime >= 30.0f) {
+            if (elapsedTime >= this.interval) {
                 elapsedTime = 0f;
                 return this.mana;
             }
         }
-    return 0;
+    return 0f;
     }
 
 }
